68ab599025025d2bc2f3c8da88a93eda360324e6
pmu15 <pmu@bifo-mbp002.fritz.box>
2024-09-14 22:33:15 +0200

    fix model ranking

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from jsonl_viewer import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('jsonl_viewer.urls')),
+    path('model_ranking/', views.model_ranking, name='model_ranking'),
 ]
